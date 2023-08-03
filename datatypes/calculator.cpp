9b4393f826ba62f029808ca478f6d0be4e48e924
Aryan Kumar <aryan.ak434@gmail.com>
2023-08-03 20:37:23 +0530

    add: idk i am going mad with streaming

---
+++ datatypes/calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int validateInput () {
+    string input;
+    list<string> validInputs{"+", "-", "*", "/", "%", "quit"};
+    // while(true) {
+    //     cin >> input;
+    //     if (input )
+    // }
+}
+
+int main() {
+    return 0;
+}
